39b722fdcd40dfc7fa98edeae9bf9aa783ff18a4
Jeroen Bos <jeroen@deepinsight.io>
2020-03-18 01:29:43 +0100

    Add status

--- app/src/main/java/com/example/bype/MainActivity.java
+++ app/src/main/java/com/example/bype/MainActivity.java
@@ -10,4 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+//    Running this shows empty activity
 }
