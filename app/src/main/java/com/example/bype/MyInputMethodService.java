88efd501d85dfc9e9113ba8538eb506b8521fe0c
Jeroen Bos <jeroen@deepinsight.io>
2020-03-30 16:28:09 +0200

    TBF reference stuff

--- app/src/main/java/com/example/bype/MyInputMethodService.java
+++ app/src/main/java/com/example/bype/MyInputMethodService.java
@@ -33,7 +33,6 @@ public class MyInputMethodService extends InputMethodService
         keyboard = new Keyboard(this, R.xml.keys_layout);
         keyboardView.setKeyboard(keyboard);
         keyboardView.setOnKeyboardActionListener(this);
-        super.onCreateInputMethodSessionInterface()
 
         return keyboardView;
     }
