699775f8a8149d2c4431735f3c9ec3ee1f92d71c
Jeroen Bos <jeroen@deepinsight.io>
2020-04-02 00:09:14 +0200

    Simplify SwipeTracker (velovities aren't necessary, and now trail isn't fixed length)

--- app/src/main/java/com/example/bype/KeyboardView.java
+++ app/src/main/java/com/example/bype/KeyboardView.java
@@ -1622,17 +1622,23 @@ public class KeyboardView extends View implements View.OnClickListener {
     // should probably be package rather than public
     public static class SwipeTracker {
 
-        static final int NUM_PAST = 4;
+        static final int INITIAL_CAPACITY = 1000;
         static final int LONGEST_PAST_TIME = 200;
 
-        final float[] mPastX = new float[NUM_PAST];
-        final float[] mPastY = new float[NUM_PAST];
-        final long[] mPastTime = new long[NUM_PAST];
+        final List<Float> mPastX = new ArrayList<Float>(INITIAL_CAPACITY);
+        final List<Float> mPastY = new ArrayList<Float>(INITIAL_CAPACITY);
+        final List<Long> mPastTime = new ArrayList<Long>(INITIAL_CAPACITY);
 
-        float mYVelocity;
-        float mXVelocity;
+        public SwipeTracker() {
+            Log.d("___________________", "Creating SwipeTracker");
+        }
 
         private int snapshotId;
+        private int trailId;
+
+        public int getLength() {
+            return this.mPastTime.size();
+        }
 
         /**
          * An id that increments on every change of the swipe trail.
@@ -1641,103 +1647,64 @@ public class KeyboardView extends View implements View.OnClickListener {
             return this.snapshotId;
         }
 
+        /**
+         * Gets a unique identifier per trail.
+         * @return
+         */
+
+        public int getTrailId() {
+            return this.trailId;
+        }
+
         public void clear() {
             this.snapshotId++;
-            mPastTime[0] = 0;
+            this.trailId++;
+            mPastTime.clear();
+            mPastX.clear();
+            mPastY.clear();
         }
 
         public void addMovement(MotionEvent ev) {
             this.snapshotId++;
-            long time = ev.getEventTime();
-            final int N = ev.getHistorySize();
-            for (int i = 0; i < N; i++) {
-                addPoint(ev.getHistoricalX(i), ev.getHistoricalY(i),
-                        ev.getHistoricalEventTime(i));
+            if (ev.getAction() == MotionEvent.ACTION_DOWN) {
+                // for now this 'breaks' when multiple pointers are shown
+                this.clear();
             }
-            addPoint(ev.getX(), ev.getY(), time);
+
+            final int N = ev.getHistorySize();
+            Log.d("history size: ", ev.getAction() + ", " + String.valueOf(N) + " at time " + ev.getEventTime());
+
+            // historical events are those for which there hasn't been a MotionEvent, since the last MotionEvent
+            for (int i = 0; i < N; i++) {
+                addPoint(
+                        ev.getHistoricalX(i),
+                        ev.getHistoricalY(i),
+                        ev.getHistoricalEventTime(i)
+                );
+            }
+            // add current event
+            addPoint(ev.getX(), ev.getY(), ev.getEventTime());
         }
 
         private void addPoint(float x, float y, long time) {
-            int drop = -1;
-            int i;
-            final long[] pastTime = mPastTime;
-            for (i = 0; i < NUM_PAST; i++) {
-                if (pastTime[i] == 0) {
-                    break;
-                } else if (pastTime[i] < time - LONGEST_PAST_TIME) {
-                    drop = i;
-                }
-            }
-            if (i == NUM_PAST && drop < 0) {
-                drop = 0;
-            }
-            if (drop == i) drop--;
-            final float[] pastX = mPastX;
-            final float[] pastY = mPastY;
-            if (drop >= 0) {
-                final int start = drop + 1;
-                final int count = NUM_PAST - drop - 1;
-                System.arraycopy(pastX, start, pastX, 0, count);
-                System.arraycopy(pastY, start, pastY, 0, count);
-                System.arraycopy(pastTime, start, pastTime, 0, count);
-                i -= (drop + 1);
-            }
-            pastX[i] = x;
-            pastY[i] = y;
-            pastTime[i] = time;
-            i++;
-            if (i < NUM_PAST) {
-                pastTime[i] = 0;
-            }
+            this.mPastTime.add(time);
+            this.mPastX.add(x);
+            this.mPastY.add(y);
         }
 
+        // TODO: remove
         public void computeCurrentVelocity(int units) {
-            computeCurrentVelocity(units, Float.MAX_VALUE);
         }
 
         public void computeCurrentVelocity(int units, float maxVelocity) {
-            final float[] pastX = mPastX;
-            final float[] pastY = mPastY;
-            final long[] pastTime = mPastTime;
-
-            final float oldestX = pastX[0];
-            final float oldestY = pastY[0];
-            final long oldestTime = pastTime[0];
-            float accumX = 0;
-            float accumY = 0;
-            int N = 0;
-            while (N < NUM_PAST) {
-                if (pastTime[N] == 0) {
-                    break;
-                }
-                N++;
-            }
-
-            for (int i = 1; i < N; i++) {
-                final int dur = (int) (pastTime[i] - oldestTime);
-                if (dur == 0) continue;
-                float dist = pastX[i] - oldestX;
-                float vel = (dist / dur) * units;   // pixels/frame.
-                if (accumX == 0) accumX = vel;
-                else accumX = (accumX + vel) * .5f;
-
-                dist = pastY[i] - oldestY;
-                vel = (dist / dur) * units;   // pixels/frame.
-                if (accumY == 0) accumY = vel;
-                else accumY = (accumY + vel) * .5f;
-            }
-            mXVelocity = accumX < 0.0f ? Math.max(accumX, -maxVelocity)
-                    : Math.min(accumX, maxVelocity);
-            mYVelocity = accumY < 0.0f ? Math.max(accumY, -maxVelocity)
-                    : Math.min(accumY, maxVelocity);
         }
 
         public float getXVelocity() {
-            return mXVelocity;
+            return 0;
         }
 
         public float getYVelocity() {
-            return mYVelocity;
+            return 0;
         }
     }
 }
