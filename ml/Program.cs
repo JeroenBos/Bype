16e9570663518bda7a76066be5970031af0d28e0
Jeroen Bos <jeroen@deepinsight.io>
2020-04-03 12:07:02 +0200

    Attempt one (won't run)

---
+++ ml/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Bype.ML
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("HI");
+        }
+    }
+}
