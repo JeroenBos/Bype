ecda8e8501e56159de735571cf76566103b88a18
Jeroen Bos <jeroen@deepinsight.io>
2020-04-04 13:23:17 +0200

    Add test_params_subclass_clone

--- tests/test_keyboard.py
+++ tests/test_keyboard.py
@@ -4,14 +4,28 @@ from python.keyboard.hp import Params
 
 class HpParamsTests(unittest.TestCase):
     def test_params_clone(self):
-        class TestParams(Params):
-            pass
-        params = TestParams()
+        params = Params()
         paramsAsDict = params.getParameters()
         assert len(paramsAsDict) == 2
         assert 'activation' in paramsAsDict
         assert 'num_epochs' in paramsAsDict
 
+    def test_params_subclass_clone(self):
+        class TestParams(Params):
+            def __init__(self,
+                         num_epochs=5,
+                         activation='relu',
+                         test_param=''):
+                super().__init__(num_epochs, activation)
+                self.test_param = test_param
+
+        params = TestParams()
+        paramsAsDict = params.getParameters()
+        assert len(paramsAsDict) == 3
+        assert 'activation' in paramsAsDict
+        assert 'num_epochs' in paramsAsDict
+        assert 'test_param' in paramsAsDict
+
 
 if __name__ == '__main__':
-    HpParamsTests().test_params_clone()
+    HpParamsTests().test_params_subclass_clone()
