359589077ec6bca2e97071dbfa7b18d48665f501
Jeroen Bos <jeroen@deepinsight.io>
2020-04-05 14:07:33 +0200

    Create 'class InMemoryDataSource(DataSource)'

--- python/model_training.py
+++ python/model_training.py
@@ -32,3 +32,19 @@ class CsvDataSource(DataSource):
         return self.df.drop([self.target_column])
 
 
+class InMemoryDataSource(DataSource):
+    def __init__(self, path: str, target_column_name_or_index: Union[int, str], *read_csv_args):
+        self.path = path
+        self.df = pd.read_csv(path, *read_csv_args)
+        x = target_column_name_or_index  # shorten name
+        if isinstance(x, str):
+            self.target_column_name = x
+        else:
+            self.target_column_name = list(self.df.columns.values)[x]
+            assert self.target_column_name is not None
+
+    def get_target(self):
+        return self.df[:, self.target_column_index]
+
+    def get_train(self):
+        return self.df.drop([self.target_column])
