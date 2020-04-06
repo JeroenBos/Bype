98192de36c77fc2c6075417cad48f93d839a5f46
Jeroen Bos <jeroen@deepinsight.io>
2020-04-06 16:56:03 +0200

    Create _3_scoring.py

--- python/keyboard/_5_pipeline.py
+++ python/keyboard/_5_pipeline.py
@@ -1,6 +1,6 @@
 from python.keyboard._1_transform import data
 from python.keyboard._2_model import KeyboardEstimator
-from python.keyboard._3_output import KeyboardResultWriter
+from python.keyboard._4_output import KeyboardResultWriter
 from python.keyboard.hp import do_hp_search
 from typing import List, Union  # noqa
 
