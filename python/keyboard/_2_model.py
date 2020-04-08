f139f09c741463954c4b5d439f294c73c249b94a
Jeroen Bos <jeroen@deepinsight.io>
2020-04-08 21:01:34 +0200

    Build combining the two input layers

--- python/keyboard/_2_model.py
+++ python/keyboard/_2_model.py
@@ -1,21 +1,36 @@
 from python.keyboard.hp import MyBaseEstimator, Model
 from typing import List, Union, Optional  # noqa
-import tensorflow as tf
-
+import tensorflow as tf  # noqa
+from tf.keras.layers import Input, Dense, LSTM, concatenate  # noqa
+from python.keyboard._2a_word_input_model import CappedWordStrategy, WordStrategy
 
 # Input to an LSTM layer always has the (batch_size, timesteps, features) shape.
 # from python.keyboard.hp import Params, MLModel
+
+
 class KeyboardEstimator(MyBaseEstimator):
-    def __init__(self, num_epochs=5, activation='relu'):
+    def __init__(self, num_epochs=5,
+                 activation='relu',
+                 swipe_feature_count=4,
+                 word_input_strategy: WordStrategy = CappedWordStrategy(5)):
         super().__init__()
         self.num_epochs = num_epochs
         self.activation = activation
+        self.swipe_feature_count = swipe_feature_count
+        self.word_input_strategy = word_input_strategy
 
     def _create_model(self) -> Model:
-        return tf.keras.Sequential([
-                   tf.keras.layers.Dense(14, activation=self.activation),
-                   tf.keras.layers.Dense(1, activation='sigmoid')
-               ])
+        # None here means variable over batches (but not within a batch)
+        swipe_input = Input(shape=(self.swipe_feature_count, None))
+        if isinstance(self.word_input_strategy, CappedWordStrategy):
+            word_input = Input(shape=(self.word_input_strategy.n, ))
+
+        swipe_dense = Dense(10, )(swipe_input)
+        word_dense = Dense(10, )(word_input)
+        merged = concatenate([swipe_dense, word_dense])
+
+        model = Model(inputs=[swipe_input, word_input], outputs=merged)
+        return model
 
     def _compile(self, model: Optional[Model] = None) -> None:
         model = model if model else self.current_model
---
+++ python/keyboard/_2a_word_input_model.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+
+class WordStrategies(ABC):
+    """
+    This specs a subpart of the model, namely the input of words.
+    Specifies that the words Input layer is of the specified fixed length.
+    """
+    pass
+
+
+class CappedWordStrategy(WordStrategies):
+    def __init__(self, n: int):
+        """:param n: The first n characters in the word will be used. """
+        assert isinstance(n, int)
+        assert n > 0
+        self.n = n
