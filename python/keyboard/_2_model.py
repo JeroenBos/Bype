2cb2e5fa1997b404209b2c0d46e47df8e4ea0bee
Jeroen Bos <jeroen@deepinsight.io>
2020-04-08 21:55:33 +0200

    Fix dimensionality bug in creating model

--- python/keyboard/_2_model.py
+++ python/keyboard/_2_model.py
@@ -12,22 +12,23 @@ class KeyboardEstimator(MyBaseEstimator):
     def __init__(self, num_epochs=5,
                  activation='relu',
                  swipe_feature_count=4,
+                 swipe_timesteps_count: int = 100,
                  word_input_strategy: WordStrategy = CappedWordStrategy(5)):
         super().__init__()
         self.num_epochs = num_epochs
         self.activation = activation
         self.swipe_feature_count = swipe_feature_count
         self.word_input_strategy = word_input_strategy
+        self.swipe_timesteps_count = swipe_timesteps_count
 
     def _create_model(self) -> Model:
         # None here means variable over batches (but not within a batch)
-        swipe_input = Input(shape=(self.swipe_feature_count, None))
+        swipe_input = Input(shape=(self.swipe_feature_count, self.swipe_timesteps_count))
         if isinstance(self.word_input_strategy, CappedWordStrategy):
-            word_input = Input(shape=(self.word_input_strategy.n, ))
+            # this means the word input is appended to every timestep in the swipe data
+            word_input = Input(shape=(self.word_input_strategy.n, self.swipe_timesteps_count))
 
-        swipe_dense = Dense(10, )(swipe_input)
-        word_dense = Dense(10, )(word_input)
-        merged = concatenate([swipe_dense, word_dense])
+        merged = concatenate([swipe_input, word_input], axis=1)
 
         model = Model(inputs=[swipe_input, word_input], outputs=merged)
         return model
