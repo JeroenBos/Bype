d809d16db405f5f5afedcbb15084a68d2e5580d3
Jeroen Bos <jeroen@deepinsight.io>
2020-04-09 16:30:03 +0200

    adorn

--- python/keyboard/_0_generate.py
+++ python/keyboard/_0_generate.py
@@ -17,6 +17,6 @@ df = DataFrame(None, columns=['words', 'swipes'], dtype=object)
 
 single_letters = [chr(i) for i in range(97, 97 + 26)]
 single_letter_words = DataFrame(single_letters, columns=['words'], dtype=str)
-single_letter_swipes = swipes_embedding_df.append(DataFrame(single_letters, columns=['X']))
+single_letter_swipes = swipes_embedding_df.append(DataFrame(single_letters, olumns=['X']))
 
 single_letters_data = Data(single_letter_words, single_letter_swipes)
