7e3f6e005e9f011a877f71e627c3c65899f2522a
Jeroen Bos <jeroen@deepinsight.io>
2020-04-10 13:45:58 +0200

    Create class InputData

--- python/keyboard/_0_generate.py
+++ python/keyboard/_0_generate.py
@@ -1,22 +1,25 @@
 # this file generates training data
 
 from python.keyboard._1_import import SPEC
-import pandas
+import pandas as pd
 from pandas import DataFrame
-from collections import namedtuple
-
-Data = namedtuple('Data', 'words swipes')
 
 
-swipes_embedding_df: DataFrame = pandas.read_csv('/home/jeroen/git/bype/data/empty.csv',
-                                                 names=SPEC.keys(),
-                                                 dtype=SPEC)
+class InputData:
+    def __init__(self, words: pd.DataFrame, swipes: pd.DataFrame):
+        self.words = words
+        self.swipes = swipes
+
+
+swipes_embedding_df: DataFrame = pd.read_csv('/home/jeroen/git/bype/data/empty.csv',
+                                             names=SPEC.keys(),
+                                             dtype=SPEC)
 
 df = DataFrame(None, columns=['words', 'swipes'], dtype=object)
 
 
 single_letters = [chr(i) for i in range(97, 97 + 26)]
 single_letter_words = DataFrame(single_letters, columns=['words'], dtype=str)
-single_letter_swipes = swipes_embedding_df.append(DataFrame(single_letters, olumns=['X']))
+single_letter_swipes = swipes_embedding_df.append(DataFrame(single_letters, columns=['X']))
 
-single_letters_data = Data(single_letter_words, single_letter_swipes)
+single_letters_data = InputData(single_letter_words, single_letter_swipes)
