ffa8ef9797146a16706f486add6a4d075a38074e
Jeroen Bos <jeroen@deepinsight.io>
2020-04-13 21:50:20 +0200

    Build automated type validation for my dfs

--- python/generic.py
+++ python/generic.py
@@ -1,6 +1,7 @@
 from typing import Any, Callable, List, Union, Dict, Type
 import pandas as pd
 from ctypes import cast
+import numpy as np
 
 
 def generic(*params: str) -> type:
@@ -38,6 +39,18 @@ def generic(*params: str) -> type:
 
 # I'm abusing this file here to mean 'utils' rather than generics
 
+def bind(instance, func, as_name=None):
+    """
+    Bind the function *func* to *instance*, with either provided name *as_name*
+    or the existing name of *func*. The provided *func* should accept the 
+    instance as the first argument, i.e. "self".
+    """
+    if as_name is None:
+        as_name = func.__name__
+    bound_method = func.__get__(instance, instance.__class__)
+    setattr(instance, as_name, bound_method)
+    return bound_method
+
 def create_empty_df(length: int, columns: Union[List[str], Dict[str, Type]], **defaults: Union[Any, Callable[[], Any]]) -> pd.DataFrame:
     """ Creates an empty df of the correct format and shape, initialized with default values, which default to 0. """
     for key, value in defaults.items():
@@ -48,7 +61,7 @@ def create_empty_df(length: int, columns: Union[List[str], Dict[str, Type]], **d
     defaults = {key: (value() if isinstance(value, Callable) else value) for key, value in defaults.items()}
 
     # pad defaults with defaults
-    if isinstance(columns, List):
+    if isinstance(columns, List):  # TODO: change signature to disallow specifying columns as list
         for column in columns:
             if column not in defaults:
                 defaults[column] = 0
@@ -67,4 +80,17 @@ def create_empty_df(length: int, columns: Union[List[str], Dict[str, Type]], **d
     result = pd.DataFrame([list(defaults[key] for key in columns) for _ in range(length)], columns=columns)
     if isinstance(columns, Dict):
         result.astype(dtype=columns, copy=False)
+    if isinstance(columns, Dict):
+        def validate(self: pd.DataFrame):
+            for column, dtype in columns.items():
+                for elem in result[column]:
+                    assert isinstance(elem, dtype)
+    else:
+        def validate(self: pd.DataFrame):
+            for column, dtype in zip(self.columns, self.dtypes):
+                if dtype.type is not np.object_:
+                    for elem in result[column]:
+                        assert isinstance(elem, dtype.type)
+    bind(result, validate)
+    result.validate()
     return result
