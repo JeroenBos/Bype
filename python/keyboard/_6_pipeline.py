6622b7abf82d4284122a97ad9e6079c7535a21e2
Jeroen Bos <jeroen@deepinsight.io>
2020-04-13 13:55:31 +0200

    Remove custom scoring

--- python/keyboard/_6_pipeline.py
+++ python/keyboard/_6_pipeline.py
@@ -2,18 +2,13 @@ from python.keyboard._0_types import SwipeEmbeddingDataFrame
 from python.keyboard._1a_generate import single_letter_swipes
 from python.keyboard._2_transform import Preprocessor
 from python.keyboard._3_model import KeyboardEstimator
-from python.keyboard._4_scoring import Scorer, MyLoss
 from python.keyboard._5_output import KeyboardResultWriter
 from python.keyboard.hp import do_hp_search
 from typing import List, Union
 
 
-data = SwipeEmbeddingDataFrame.__as__(single_letter_swipes)
+data = SwipeEmbeddingDataFrame.__as__(single_letter_swipes).convolve()
 
-scorer = Scorer(data)
-loss_fn = MyLoss(data)
-
-preprocessor = Preprocessor(loss_ctor=loss_fn)
+preprocessor = Preprocessor()
 
 training = KeyboardEstimator[preprocessor].create_initialized().fit(data)
-
