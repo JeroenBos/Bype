ffa810ecf51763cb48ea60130290fc15f814818f
Jeroen Bos <jeroen@deepinsight.io>
2020-04-16 20:04:43 +0200

    Build timer in pipeline

--- python/keyboard/_6_pipeline.py
+++ python/keyboard/_6_pipeline.py
@@ -1,11 +1,19 @@
 from keyboard._0_types import SwipeEmbeddingDataFrame
-from keyboard._1a_generate import single_letter_swipes, double_letter_swipes, single_and_double_letter_swipes  # noqa
+from keyboard._1a_generate import single_letter_swipes, double_letter_swipes, single_and_double_letter_swipes, single_double_and_triple_letter_swipes  # noqa
 from keyboard._2_transform import Preprocessor
 from keyboard._3_model import KeyboardEstimator
 from typing import List, Union
+from time import time
 
-data = SwipeEmbeddingDataFrame.__as__(single_and_double_letter_swipes()).convolve()
+starttime = time()
 
-preprocessor = Preprocessor(max_time_steps=2)
+data = SwipeEmbeddingDataFrame.__as__(single_double_and_triple_letter_swipes())
+print(f'generating data took {time() - starttime} seconds')
+starttime = time()
+
+data = data.convolve()
+print(f'convolving took {time() - starttime} seconds')
+
+preprocessor = Preprocessor(max_time_steps=3)
 
 training = KeyboardEstimator[preprocessor].create_initialized(num_epochs=10).fit(data)
