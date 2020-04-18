b98a1ac257781a4e14f8091940d1ded20ab3a81c
Jeroen Bos <jeroen@deepinsight.io>
2020-04-18 11:22:43 +0200

    Convolve validation data

--- python/keyboard/_6_pipeline.py
+++ python/keyboard/_6_pipeline.py
@@ -1,6 +1,7 @@
 from keyboard._0_types import SwipeEmbeddingDataFrame
 from keyboard._1a_generate import single_letter_swipes, double_letter_swipes, single_and_double_letter_swipes, single_double_and_triple_letter_swipes  # noqa
 from keyboard._2_transform import Preprocessor
+from keyboard._3_scoring import Metrics
 from keyboard._4_model import KeyboardEstimator
 from typing import List, Union
 from time import time
@@ -12,7 +13,7 @@ data: SwipeEmbeddingDataFrame = SwipeEmbeddingDataFrame.__as__(single_double_and
 print(f'generating data took {time() - starttime} seconds')
 
 starttime = time()
-data = data.convolve(verify)
+convolved_data = data.convolve(verify)
 print(f'convolving took {time() - starttime} seconds')
 
 preprocessor = Preprocessor(max_timesteps=3)
