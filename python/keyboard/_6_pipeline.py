4f3f2342608d43f15bd02e436ec20d75c1710da7
Jeroen Bos <jeroen@deepinsight.io>
2020-04-20 22:56:14 +0200

    Revert "Revert "Custom lr""
    This reverts commit 78aef4a98347ef47b8af4b35747ab7f1b527566d.

--- python/keyboard/_6_pipeline.py
+++ python/keyboard/_6_pipeline.py
@@ -6,11 +6,25 @@ from keyboard._3_scoring import Metrics
 from keyboard._4_model import KeyboardEstimator
 from typing import List, Union
 from time import time
+from tensorflow.keras.callbacks import LearningRateScheduler  # noqa
 
 preprocessor = Preprocessor(max_timesteps=3)
 
 metric = Metrics(preprocessor.preprocess(convolved_data), preprocessor.decode, convolved_data.get_i, len(data))
+def adapt_learning_rate(epoch: int) -> float:
+    if epoch < 10:
+        return 0.01
+    elif epoch < 20:
+        return 0.001
+    elif epoch < 80:
+        return 0.0001
+    else:
+        return 0.00001
 
-training = KeyboardEstimator[preprocessor].create_initialized(num_epochs=1000)  \
-                                          .with_callback(metric)               \
+
+lr_scheduler = LearningRateScheduler(adapt_learning_rate)
+
+
+training = KeyboardEstimator[preprocessor].create_initialized(num_epochs=100)    \
+                                          .with_callback(metric, lr_scheduler)   \
                                           .fit(convolved_data)
