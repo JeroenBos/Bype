aa82a99b008eff2b7ebe3aae57c96f6b21b33412
Jeroen Bos <jeroen@deepinsight.io>
2020-04-20 13:26:56 +0200

    Show misinterpretations from loaded model

--- python/keyboard/_5_output.py
+++ python/keyboard/_5_output.py
@@ -1,22 +1,25 @@
+import os
 from tensorflow.keras.models import load_model, Model  # noqa
 from MyBaseEstimator import get_log_dir
 from os import path
 import numpy as np
-from keyboard._0_types import SwipeEmbeddingDataFrame
+from keyboard._0_types import SwipeEmbeddingDataFrame, ProcessedInputSeries
+from keyboard._1a_generate import generated_convolved_data as convolved_data, generated_data as data
 from keyboard._2_transform import Preprocessor
+from keyboard._3_scoring import Metrics
 from keyboard._4_model import KeyboardEstimator
 from keyboard.MyModelCheckpoint import get_processor_path
 import json
 from myjson import json_decoders
 from utilities import memoize
 
-most_recent_modelpath = get_log_dir('logs') + path.sep + 'model.h5'
+most_recent_modelpath = get_log_dir('logs/') + path.sep + 'model.h5'
 
 
 def load_model_with_preprocessor(model_path: str = most_recent_modelpath) -> "LoadedKeyboardModel":
     if not os.path.exists(model_path):
         raise ValueError(f"Path '{model_path}' doesn't exist")
-    
+
 
     with open(get_processor_path(model_path)) as file:    
         preprocessor_json = json.load(file)
@@ -32,14 +35,18 @@ class LoadedKeyboardModel:
         self.model = model
 
 
-    def _preprocess(self, X: SwipeEmbeddingDataFrame) -> np.ndarray:
+    def predict(self, preprocessedX: ProcessedInputSeries) -> np.ndarray:
+        return self.model.predict(preprocessedX)
+
+
+    def _preprocess(self, X: SwipeEmbeddingDataFrame) -> ProcessedInputSeries:
         if self.preprocessor is None:
             return X
         return self.preprocessor._preprocess(X)
 
-    def predict(self, X: SwipeEmbeddingDataFrame) -> np.ndarray:
+    def preprocess_and_predict(self, X: SwipeEmbeddingDataFrame) -> np.ndarray:
         preprocessedX = self._preprocess(X)
-        return self.model.predict(preprocessedX)
+        return self.predict(preprocessedX)
 
 
 @memoize
@@ -47,4 +54,11 @@ def most_recent_model():
     return load_model_with_preprocessor(most_recent_modelpath)
 
 
-class Scoring:
+model = load_model_with_preprocessor(os.getcwd() + '/logs/2020_04_18/model.h5')
+model.preprocessor.save(os.getcwd() + '/logs/2020_04_18/model_copy.json')
+
+preprocessor = Preprocessor(max_timesteps=2)
+
+metric = Metrics(preprocessor.preprocess(convolved_data), preprocessor.decode, convolved_data.get_i, len(data), model=model)
+
+metric.print_misinterpreted_words()
