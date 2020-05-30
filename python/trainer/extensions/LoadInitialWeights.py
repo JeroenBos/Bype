6802889b9441d2e0c1c92df93a4fcb944c67f9fd
Jeroen Bos <jeroen@deepinsight.io>
2020-05-31 01:06:33 +0200

    Allow to specify other initial weights file on first stage

--- python/trainer/extensions/LoadInitialWeights.py
+++ python/trainer/extensions/LoadInitialWeights.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 import logging
 import os
 from tensorflow.keras.models import load_model, Model  # noqa
-from typing import Callable, Optional, Tuple
+from typing import Callable, Optional, Tuple, Union
 
 from KerasModelPadder import copy_weights
 from trainer.types import TrainerExtension, IModel
@@ -10,7 +10,14 @@ from utilities import override
 
 
 class LoadInitialWeightsTrainerExtension(TrainerExtension):
-    def __init__(self, on_first_stage=True):
+    def __init__(self, on_first_stage: Union[bool, str] = True):
+        """ 
+        :param on_first_stage: Depending on the value, determines what is loaded in the first stage:
+        - a string: is read as path and loaded
+        - True: params.initial_weights_path (like all other stages)
+        - False: nothing
+        """
+        assert isinstance(on_first_stage, (str, bool))
         self._on_first_stage = on_first_stage
 
     @property
@@ -22,8 +29,8 @@ class LoadInitialWeightsTrainerExtension(TrainerExtension):
         assert model is not None, "A model must exist to load weights into it"
         assert self._initial_weights_path is not None, "No initial weights path provided"
 
-        if not self.is_first_stage or self._on_first_stage:
-            path = self._initial_weights_path
+        if not self.is_first_stage or self._on_first_stage is not False:
+            path = self._on_first_stage if isinstance(self._on_first_stage, str) else self._initial_weights_path
             try:
                 if not os.path.exists(path):
                     logging.error(f"File '{path}' does not exist")
