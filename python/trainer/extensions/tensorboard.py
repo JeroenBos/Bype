50aeea8e64476cf08f21a03e66425250ab8f87f4
Jeroen Bos <jeroen@deepinsight.io>
2020-05-31 01:19:59 +0200

    Fix error "Must enable trace before export."

--- python/trainer/extensions/tensorboard.py
+++ python/trainer/extensions/tensorboard.py
@@ -6,5 +6,8 @@ from utilities import override
 class TensorBoardExtension(TrainerExtension):
     def initialize(self):
         self.params.fit_args.callbacks.append(
-            TensorBoard(log_dir=self.params.log_dir, histogram_freq=1)
+            TensorBoard(log_dir=self.params.log_dir, 
+                        histogram_freq=1,
+                        profile_batch=100000000,  # prevents error "Must enable trace before export."
+                        )
         )
