b109de84ab209c212789336b52c2fa03ebf1d2bc
Jeroen Bos <jeroen@deepinsight.io>
2020-06-06 15:36:40 +0200

    Compute SaveBestModel filepath lazily

--- python/trainer/extensions/SaveBestModel.py
+++ python/trainer/extensions/SaveBestModel.py
@@ -1,8 +1,9 @@
 from dataclasses import dataclass
-from typing import Callable
+from typing import Callable, Union
 from trainer.trainer import TrainerExtension, Trainer
 from tensorflow.keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping  # noqa
 from utilities import override
+import os
 
 
 @dataclass
@@ -14,39 +15,44 @@ class Params:
 
 class SaveBestModelTrainerExtension(TrainerExtension):
     def __init__(self, 
-                 filepath: str,         # passed to ModelCheckpoint.__init__
-                 monitor='val_loss',    # passed to ModelCheckpoint.__init__
-                 mode='auto',           # passed to ModelCheckpoint.__init__
+                 filepath: Union[str, Callable[[Params], str]],   # passed to ModelCheckpoint.__init__
+                 monitor='val_loss',                              # passed to ModelCheckpoint.__init__
+                 mode='auto',                                     # passed to ModelCheckpoint.__init__
                  best_over_all_stages=True,
                  ):
-        assert isinstance(filepath, str) 
-        assert filepath.endswith('.h5')
+        assert isinstance(filepath, (str, Callable)) 
 
         self._best_over_all_stages = best_over_all_stages
         self._kw = {
-            "filepath": filepath,
+            "filepath_lambda": filepath if isinstance(filepath, Callable) else lambda p: filepath,
             "monitor": monitor,
             "mode": mode,
             "save_best_only": True,
         }
 
     def _find_previous_callback(self):
-
+        # what is the point of this again? Shouldn't it be `self.prev_params` btw?
         return next((callback for callback in self.params.fit_args.callbacks 
                      if isinstance(callback, _SaveBestModelCallback) and callback.filepath == self._kw.get("filepath", "")), None)
 
     def initialize(self):
+        self._invoke_filepath_lambda()
         callback = None
         if self._best_over_all_stages:
             callback = self._find_previous_callback()
             if callback is None and not self.is_first_stage:
                 print("Failed to find previous save_best_model_callback")
 
-
         if callback is None:
             callback = _SaveBestModelCallback(**self._kw)
 
         self.params.fit_args.callbacks.append(callback)
+
+    def _invoke_filepath_lambda(self):
+        self._kw["filepath"] = self._kw["filepath_lambda"](self.params)
+        assert self._kw["filepath"].endswith(".h5"), "Should end with .h5"
+        del self._kw["filepath_lambda"]
+
 
 
 class _SaveBestModelCallback(ModelCheckpoint):
